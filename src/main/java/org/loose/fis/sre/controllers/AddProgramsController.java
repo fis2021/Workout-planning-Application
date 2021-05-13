b864ee673d553a329c3936a7e1542793e08ed982
TeodoraPn <iteodoora@yahoo.com>
2021-05-13 16:31:12 +0300

    Update design

--- src/main/java/org/loose/fis/sre/controllers/AddProgramsController.java
+++ src/main/java/org/loose/fis/sre/controllers/AddProgramsController.java
@@ -8,31 +8,31 @@ package org.loose.fis.sre.controllers;
         import javafx.scene.control.TextField;
         import javafx.stage.Stage;
         import org.loose.fis.sre.exceptions.ProgramAlreadyExistsException;
-        import org.loose.fis.sre.services.ProductNameService;
+        import org.loose.fis.sre.services.ProgramService;
 
         import java.io.IOException;
 
 public class AddProgramsController {
 
     @FXML
-    private ChoiceBox category;
+    private ChoiceBox intensity;
 
     @FXML
     private TextField name;
 
-    @FXML
-    private TextField price;
 
+    @FXML
+    private TextField duration;
 
     @FXML
     public void initialize() {
-        category.getItems().addAll("", "", "");
+        intensity.getItems().addAll("Beginner", "Medium", "High-intensity");
     }
 
     public void handleAddButtonAction() throws  IOException {
 
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("viewProductsManager.fxml"));
-        Stage window = (Stage) category.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Coach_viewPrograms.fxml"));
+        Stage window = (Stage) intensity.getScene().getWindow();
         window.setScene(new Scene(root, 800, 600));
 
     }
@@ -40,8 +40,8 @@ public class AddProgramsController {
 
     public void handleCancelButtonAction() throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("viewProductsManager.fxml"));
-        Stage window = (Stage) category.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Coach_viewPrograms.fxml"));
+        Stage window = (Stage) intensity.getScene().getWindow();
         window.setScene(new Scene(root, 800, 600));
     }
 
