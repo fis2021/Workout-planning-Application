23834ddefcba658e80292e91ea08bb031301db02
TeodoraPn <iteodoora@yahoo.com>
2021-05-14 17:41:34 +0300

    Final changes on buttons

--- src/main/java/org/loose/fis/sre/controllers/AddProgramsController.java
+++ src/main/java/org/loose/fis/sre/controllers/AddProgramsController.java
@@ -1,48 +1,17 @@
 package org.loose.fis.sre.controllers;
 
-        import javafx.fxml.FXML;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.scene.control.ChoiceBox;
-        import javafx.scene.control.TextField;
-        import javafx.stage.Stage;
-        import org.loose.fis.sre.exceptions.ProgramAlreadyExistsException;
-        import org.loose.fis.sre.services.ProgramService;
+        import org.loose.fis.sre.Main;
 
         import java.io.IOException;
 
 public class AddProgramsController {
 
-    @FXML
-    private ChoiceBox intensity;
 
-    @FXML
-    private TextField name;
-
-
-    @FXML
-    private TextField duration;
-
-    @FXML
-    public void initialize() {
-        intensity.getItems().addAll("Beginner", "Medium", "High-intensity");
-    }
-
-    public void handleAddButtonAction() throws  IOException {
-
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Coach_viewPrograms.fxml"));
-        Stage window = (Stage) intensity.getScene().getWindow();
-        window.setScene(new Scene(root, 800, 600));
-
+    public void handleBackButton() throws IOException{
+        Main x = new Main();
+        x.changeWindow("viewPrograms.fxml");
     }
 
 
-    public void handleCancelButtonAction() throws Exception{
-
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Coach_viewPrograms.fxml"));
-        Stage window = (Stage) intensity.getScene().getWindow();
-        window.setScene(new Scene(root, 800, 600));
-    }
 
 }
