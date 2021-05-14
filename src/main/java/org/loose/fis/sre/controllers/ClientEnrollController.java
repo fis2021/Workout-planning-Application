8a3b0723127406aa0e757ed49864eca3fa3a28ae
OlahDavid7 <olahdavid20@yahoo.com>
2021-05-14 17:16:16 +0300

    Update ClientEnroll.fxml Update ClientEnrollController.java

--- src/main/java/org/loose/fis/sre/controllers/ClientEnrollController.java
+++ src/main/java/org/loose/fis/sre/controllers/ClientEnrollController.java
@@ -1,14 +1,25 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import org.loose.fis.sre.Main;
 
 import java.io.IOException;
 
 public class ClientEnrollController {
 
+    @FXML
+    private TextField enroll;
+
     public void handleBackButton() throws IOException{
         Main x = new Main();
         x.changeWindow("ClientViewPrograms.fxml");
+    }
+
+    public void handleEnrolledButton(){
+        enroll.setText("Successfully enrolled!");
+        enroll.setStyle("-fx-text-fill: red");
     }
 
 }
