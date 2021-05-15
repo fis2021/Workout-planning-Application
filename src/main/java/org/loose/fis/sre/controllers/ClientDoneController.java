bf13c12c330e122880736c2d6dc8fbfd0595acc5
TeodoraPn <iteodoora@yahoo.com>
2021-05-15 17:59:45 +0300

    Add  programs feature updateeeee

--- src/main/java/org/loose/fis/sre/controllers/ClientDoneController.java
+++ src/main/java/org/loose/fis/sre/controllers/ClientDoneController.java
@@ -34,16 +34,6 @@ public class ClientDoneController {
     public void handleDoneButton() {
         String thisname = User.getCurrentUser();
 
-        if (ProgramService.findClient(ProgramService.getProgramName(prognamefield.getText()),thisname)==-1)
-        {
-            done.setText("You're not enrolled to this program");
-        }
-        else
-        {
-            done.setText(ProgramService.workout(ProgramService.getProgramName(prognamefield.getText()),ProgramService.findClient(ProgramService.getProgramName(programnemfield.getText()),thisname)));
-        }
-        done.setStyle("-fx-text-fill: red");
-
     }
 
 
